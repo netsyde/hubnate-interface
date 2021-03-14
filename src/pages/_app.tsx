db24e79fc027f262fd768711d0ea53bf4815e385
Sgoldik <sgoldik@mail.ru>
2021-03-14 15:19:53 +0600

    Add routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/index.scss";
 import Main from './index'
+import Donate from './donate'
 
-export default function MyApp() {
-    return <Main />
-  }
+
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default App;
---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Donate = () => {
+    return (
+        <div>
+            <p>DONATE PAGE</p>
+        </div>
+    )
+}
+
+export default Donate;
